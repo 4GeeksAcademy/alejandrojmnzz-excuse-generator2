ccf08e93bc05fcb05753327a05d4f1874ea6eabf
alejandrojmnzz <alejandro12850mj@gmail.com>
2024-10-09 03:46:47 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The neighbor ", "My little brother ", "My boss ", "The dog "];
+  let action = ["ate ", "was hit by ", "attacked ", "wanted "];
+  let what = ["my homework ", "a butterfly ", "the hose ", "my mom "];
+  let when = ["today", "yesterday", "23 seconds ago", "on Friday"];
+
+  let randomWho = who[Math.floor(Math.random() * 4)];
+  let randomAction = action[Math.floor(Math.random() * 4)];
+  let randomWhat = what[Math.floor(Math.random() * 4)];
+  let randomWhen = when[Math.floor(Math.random() * 4)];
+
+  let excuse = document.getElementById("excuse");
+
+  excuse.innerHTML = randomWho + randomAction + randomWhat + randomWhen;
 };
